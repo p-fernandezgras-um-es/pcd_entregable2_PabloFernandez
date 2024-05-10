ad5aad7f8fe08c470ef879247b5fbeb26c60d3c3
p-fernandezgras-um-es <p.fernandezgras@um.es>
2024-05-10 10:54:33 +0200

    Continuamos con el entregable

--- Pruebas_entregable_2.py
+++ Pruebas_entregable_2.py
@@ -1,13 +1,26 @@
 from entregable_2 import *
 
+datos=[
+("2024-01-17 00:00:00, 25.5"),
+("2024-01-17 01:00:00, 24.8"),
+("2024-01-17 02:00:00, 23.9"),
+("2024-01-17 03:00:00, 22.7"),
+("2024-01-17 04:00:00, 21.3"),
+("2024-01-17 05:00:00, 20.1"),
+("2024-01-17 06:00:00, 19.8"),
+("2024-01-17 07:00:00, 20.5"),
+("2024-01-17 08:00:00, 21.9")]
+sistema=Sistema.obtener_instancia()
+sensor=Observable_Sensor("Sensor")
+sensor.añadir_observer(sistema)
+for i in datos:
+    sensor.nuevo_dato(Datos(i))
+
 calcular_estadisticos=Calcular_estadisticos()
 supera_umbral=Supera_umbral()
 aumento_brusco=Aumento_brusco()
 
 calcular_estadisticos.set_proxima_operacion(supera_umbral)
 supera_umbral.set_proxima_operacion(aumento_brusco)
-
-
 problema=Operaciones(Paso)
-
-calcular_estadisticos.realizar_operacion(problema)
+calcular_estadisticos.realizar_operacion(problema,sistema)
--- entregable_2.py
+++ entregable_2.py
@@ -2,6 +2,16 @@ from abc import ABC, abstractmethod
 import numpy as np
 from functools import reduce
 from enum import IntEnum
+from functools import reduce
+from math import sqrt
+
+class Datos:
+    def __init__(self,tupla) -> tuple:
+
+        timestamp,temperatura=tupla.split(",")
+        self.temperatura=float(temperatura)
+        self.dia,self.hora=timestamp.split(" ")
+
 
 ## R1: Singleton
 class Sistema:
@@ -9,8 +19,16 @@ class Sistema:
 
     def __init__(self):
         self.datos=[]
+        self.umbral=30
          
-         
+    def cambiar_umbral(self,umbral:float):
+        self.umbral=umbral
+
+    def temperaturas(self):
+        temperaturas=[]
+        for dato in self.datos:
+            temperaturas.append(dato.temperatura)
+        return temperaturas
 
     @classmethod
     def obtener_instancia(cls):
@@ -20,6 +38,7 @@ class Sistema:
     
     def actualizar(self,dato):
         self.datos.append(dato)
+
 
     
 ## R2: Observer
@@ -68,40 +87,117 @@ class Manejador(ABC):
     def set_proxima_operacion(self,operacion):
         self._proxima_operacion=operacion
     
-    def realizar_operacion(self,operaciones:Operaciones):
+    def realizar_operacion(self,operaciones:Operaciones,datos):
         if self._paso_actual==(operaciones.pasos and self._paso_actual):
-            self._operar(operaciones)
+            self._operar(operaciones,datos)
         
         if (operaciones.terminado() or self._proxima_operacion==None):
             return
         else:
-            self._proxima_operacion.realizar_operacion(operaciones)
+            self._proxima_operacion.realizar_operacion(operaciones,datos)
 
     @abstractmethod 
-    def _operar(self,operacion:Operaciones):
+    def _operar(self,operacion:Operaciones,datos):
         raise NotImplementedError
     
 class Calcular_estadisticos(Manejador):
     def __init__(self) -> None:
         self._paso_actual = Paso.Calcular_estadisticos
         self._proxima_operacion = None
+        # Asignamos el estadístico media por defecto.
+        self.context=Context(Media)
     
-    def _operar(self, operacion: Operaciones):
-        print("Calculando estadisticos")
+    # Función para cambiar el estadístico.
+    def cambiar_estadistico(self,estadistico):
+        self.context.seleccionar_estadistico(estadistico)
+    
+    def _operar(self,operacion: Operaciones,sistema:Sistema):
+        print("Calculando estadisticos:")
+        return self.context.aplicar(sistema)
 
 class Supera_umbral(Manejador):
     def __init__(self) -> None:
         self._paso_actual = Paso.Supera_umbral
         self._proxima_operacion = None
-    
-    def _operar(self, operacion: Operaciones):
-        print("Supera umbral")
+ 
+    def _operar(self, operacion: Operaciones,sistema:Sistema):
+        supera_umbral=False
+        print(f"--------------------------------------------------------\nComprobando si la temperatura supera el umbral de {sistema.umbral}ºC:")
+        if sistema.datos[-1].temperatura > sistema.umbral:
+            supera_umbral=True
+            print(f"¡¡¡PELIGRO!!!")
+            print(f"La temperatura el dia {sistema.datos[-1].dia} a las {sistema.datos[-1].hora} es de {sistema.datos[-1].temperatura} y supera el umbral de {sistema.umbral}ºC")
+        else:
+            print(f"Temperatura en orden: {sistema.datos[-1].temperatura}ºC")
+        return supera_umbral
+
 
 class Aumento_brusco(Manejador):
     def __init__(self) -> None:
         self._paso_actual = Paso.Aumento_brusco
         self._proxima_operacion = None
     
-    def _operar(self, operacion: Operaciones):
-        print("Aumento brusco")
+    def _operar(self, operacion:Operaciones,sistema:Sistema):
+        print("--------------------------------------------------------\nComprobando si la temperatura ha aumentado más de 10ºC en los últimos 30 segundos:")
+        aumenta=False
+        if len(sistema.temperaturas())>=6:
+            if (sistema.temperaturas()[-1] - sistema.temperaturas()[-6]) > 10:
+                aumenta=True
+                print(f"De la hora {sistema.datos[-6].hora} a la hora {sistema.datos[-6].hora} del día {sistema.datos[-6].dia} ha aumentado la temperatura más de 10ºC ({sistema.datos[-1].temperatura - sistema.datos[-6].temperatura}ºC)")
+            else:
+                print(f"Temperatura en orden, no ha aumentado más de 10ºC en los últimos 30 segundos")
         
+        else:
+            print("Todavía no hay datos de lo últimos 30 segundos")
+        return aumenta
+
+## R4: Strategy
+
+class Context:
+    def __init__(self,estadistico) -> None:
+        self.estadistico=estadistico
+
+    def seleccionar_estadistico(self,estadistico):
+        self.estadistico=estadistico
+    
+    def aplicar(self,sistema:Sistema):
+        return self.estadistico.calcular_estadistico(self.estadistico,sistema)
+
+class Estadistico(ABC):
+
+    @abstractmethod
+    def calcular_estadistico(self,sistema:Sistema):
+        pass
+
+class Media(Estadistico):
+    
+    def calcular_estadistico(self,sistema:Sistema):
+        # calculamos la media y la desviación típica haciendo uso de la programación funcional.
+        media= reduce(lambda x, y: x + y, sistema.temperaturas()[-12:]) / len(sistema.temperaturas()[-12:])
+        dt=sqrt(sum((x - media) ** 2 for x in sistema.temperaturas()) / len(sistema.temperaturas()))
+
+        print(f"La temperatura media en los últimos 60 segundos es de: {round(media,2)}ºC y la deviación típica es de {round(dt,2)}ºC.")
+        return media,dt
+    
+class Cuantiles(Estadistico):
+
+    def calcular_estadistico(self, sistema: Sistema):
+        # Utilizamos la funcion np.quantile para calcular los diferentes cuantiles,
+        primer_cuantil=np.quantile(0.25,sistema.temperaturas())
+        segundo_cuantil=np.quantile(0.5,sistema.temperaturas())
+        tercer_cuantil=np.quantile(0.75,sistema.temperaturas())
+
+        print(f"Primer cuantil: {primer_cuantil}ºC.\nSegundo cuantil: {segundo_cuantil}ºC.\nTercer cuantil: {tercer_cuantil}ºC.")
+
+        return primer_cuantil,segundo_cuantil,tercer_cuantil
+
+class MaxMIn(Estadistico):
+
+    def calcular_estadistico(self, sistema: Sistema):
+        #Hallamos la temperatura máxima y mínima utilizando la programación funcional.
+        t_max=reduce(lambda a, b: a if a > b else b, sistema.temperaturas()[-12:])
+        t_min=reduce(lambda a, b: a if a < b else b, sistema.temperaturas()[-12:])
+
+        print(f"Temperatura máxima y mínima alcanzada en los últimos 60 segundos:\nTemperatura máxima: {t_max}ºC\nTemperatura máxima: {t_min}ºC")
+        return t_max,t_min
+    
